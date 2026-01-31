5da0b17cacd6aaf1b8cb68326898c6eaeb8bda3c
Venkat <ravikumar.venkat@gmail.com>
2026-01-31 13:23:04 +0530

    Selenium update with chrome

--- Jenkins/Cont_Test/src/LoginTest.java
+++ Jenkins/Cont_Test/src/LoginTest.java
@@ -1,13 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginTest {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        // Selenium Manager will automatically handle ChromeDriver
+        ChromeOptions options = new ChromeOptions();
 
-        WebDriver driver = new ChromeDriver();
+        // Required for Jenkins / Linux / CI environments
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.google.com");
 
         System.out.println("Page Title: " + driver.getTitle());
